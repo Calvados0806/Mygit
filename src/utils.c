f5782b261c599d3fd08d895a6052e791b8dc6875
Calvados0806 <flamaster0806@gmail.com>
2019-03-18 16:31:14 +0200

    rewritten functions, realized struct DirInfo interface

--- src/utils.c
+++ src/utils.c
@@ -3,65 +3,75 @@
 #include <stdlib.h>
 #include <string.h>
 
-#if defined(_WIN32) || defined(_WIN64)
 
-#define MAX_PATH_LEN MAX_PATH
-
-#elif defined(__linux__) || defined(__unix__)
-
-#include <linux/limits.h>
-#include <sys/stat.h>
-#define MAX_PATH_LEN _POSIX_PATH_MAX 
-
-#endif // _WIN32, _WIN64
-
-static char **Realloc(char **src, size_t nElems, size_t *outNewNelems)
+static void InitDirInfo(struct DirInfo *obj)
 {
-	size_t newSize = (size_t)(nElems * 1.68);
-	char **newBuff = (char **)malloc(newSize * sizeof(char *));
-	memcpy(newBuff, src, sizeof(char *) * nElems);
-	free(src);
+	obj->Amount = 0;
+	obj->Capacity = 10;
+	obj->Names = (char **)malloc(sizeof(char **) * obj->Capacity);
+	*(obj->Names) = (char *)malloc(sizeof(char[MAX_PATH_LEN]) * obj->Capacity);
 
-	char *newContentBuff = (char *)malloc(MAX_PATH_LEN * newSize * sizeof(char));
-	memcpy(newContentBuff, *newBuff, MAX_PATH_LEN * nElems * sizeof(char));
-	free(*newBuff);
-	*newBuff = newContentBuff;
-
-	for (size_t i = 1; i < newSize; i++) {
-		newBuff[i] = newBuff[i - 1] + MAX_PATH_LEN;
-	}
-
-	*outNewNelems = newSize;
-	return newBuff;
+	for (int i = 1; i < obj->Capacity; i++)
+		obj->Names[i] = obj->Names[i - 1] + MAX_PATH_LEN;
 }
 
 
-char **GetFileNames(const char *const dirName, size_t *outFileCount)
+static void Realloc(struct DirInfo *obj)
 {
-	size_t elemCount = 10, fileCounter = 0;
-	char **fileNames = (char **)malloc(elemCount * sizeof(char *));
-	*fileNames = (char *)malloc(MAX_PATH_LEN * elemCount * sizeof(char));
+	int newSize = (int)(obj->Capacity * 1.68);
+	char **newBuff = (char **)malloc(sizeof(char **) * newSize);
+	memcpy(newBuff, obj->Names, sizeof(char **) * obj->Capacity);
+	free(obj->Names);
+	obj->Names = newBuff;
 
-	for (size_t i = 1; i < elemCount; i++) {
-		fileNames[i] = fileNames[i-1] + MAX_PATH_LEN;
-	}
+	char *newContentBuff = (char *)malloc(sizeof(char[MAX_PATH_LEN]) * newSize);
+	memcpy(newContentBuff, *(obj->Names), sizeof(char[MAX_PATH_LEN]) * obj->Capacity);
+	free(*(obj->Names));
+	*(obj->Names) = newContentBuff;
+
+	for (int i = 1; i < newSize; i++)
+		obj->Names[i] = obj->Names[i - 1] + MAX_PATH_LEN;
+	obj->Capacity = newSize;
+}
+
+
+void FreeDirInfo(struct DirInfo *obj)
+{
+	free(*(obj->Names));
+	free(obj->Names);
+	free(obj);
+}
+
+
+const char *FileName(struct DirInfo *obj, int index)
+{
+	if (index >= 0 && index < obj->Amount)
+		return obj->Names[index];
+	return NULL;
+}
+
+
+struct DirInfo *GetFileNames(const char *const path)
+{
+	struct DirInfo *info = (struct DirInfo *)malloc(sizeof(struct DirInfo));
+	InitDirInfo(info);
+
 #if defined(_WIN32) || defined(_WIN64)
 	HANDLE handler;
 	WIN32_FIND_DATA fileInfo;
 
-	handler = FindFirstFile(TEXT(dirName), &fileInfo);
+	handler = FindFirstFile(TEXT(path), &fileInfo);
 	if (handler != INVALID_HANDLE_VALUE) {
 		do {
-			if (fileCounter == elemCount)
-				fileNames = Realloc(fileNames, elemCount, &elemCount);
+			if (info->Amount == info->Capacity)
+				Realloc(info);
 
 			if (strcmp(".", fileInfo.cFileName) != 0 && strcmp("..", fileInfo.cFileName) != 0)
-				strcpy_s(fileNames[fileCounter++], MAX_PATH, fileInfo.cFileName);
+				strcpy_s(info->Names[info->Amount++], MAX_PATH_LEN, fileInfo.cFileName);
 		} while (FindNextFile(handler, &fileInfo));
 
-		*outFileCount = fileCounter;
 		FindClose(handler);
-		return fileNames;
+		return info;
 	}
 	else {
 		FindClose(handler);
@@ -73,15 +83,14 @@ char **GetFileNames(const char *const dirName, size_t *outFileCount)
 
 	if ((dir = opendir(dirName)) != NULL) {
 		while ((ent = readdir(dir)) != NULL) {
-			if (fileCounter == elemCount)
-				elemCount = Realloc(fileNames, elemCount);
+			if (info->Amount == info->Capacity)
+				Realloc(info);
 
 			if (strcmp(".", ent->d_name) != 0 && strcmp("..", ent->d_name) != 0)
-				strcpy(fileNames[fileCounter++], ent->d_name);
+				strcpy(info->Names[info->Amount++], ent->d_name);
 		}
 		closedir(dir);
-		*outFileCount = fileCounter;
-		return fileNames;
+		return info;
 	}
 	else {
 		closedir(dir);
@@ -89,6 +98,7 @@ char **GetFileNames(const char *const dirName, size_t *outFileCount)
 	}
 #endif // _WIN32, _WIN64
 }
+
 
 int IsDirectory(const char *const path)
 {
@@ -105,8 +115,8 @@ int IsDirectory(const char *const path)
 	int status;
 
 	status = stat(path, &stBuff);
-	if (status != 0)
-		return -1;
-	return S_ISDIR(stBuff.st_mode);
+	if (status == 0)
+		return S_ISDIR(stBuff.st_mode);
+	return -1;
 #endif
 }
--- src/utils.h
+++ src/utils.h
@@ -4,10 +4,14 @@
 #if defined(_WIN32) || defined(_WIN64)
 
 #include <Windows.h>
+#define MAX_PATH_LEN MAX_PATH
 
 #elif defined(__linux__) || defined(__unix__)
 
 #include <dirent.h>
+#include <linux/limits.h>
+#include <sys/stat.h>
+#define MAX_PATH_LEN _POSIX_PATH_MAX 
 
 #else
 
@@ -15,11 +19,21 @@
 
 #endif // _WIN32, _WIN64
 
+struct DirInfo {
+	int Amount;
+	int Capacity;
+	char **Names;
+};
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-char **GetFileNames(const char *const dirName, size_t *outFileCount);
+void FreeDirInfo(struct DirInfo *obj);
+
+const char *FileName(struct DirInfo *obj, int index);
+
+struct DirInfo *GetFileNames(const char *const path);
 
 int IsDirectory(const char *const path);
 
