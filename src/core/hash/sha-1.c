f63bf219f189887df845030702e2c61d02c6534e
Calvados <flamaster0806@gmail.com>
2019-03-27 17:20:43 +0200

    changed 8-bit values to 32-bit values in uint160_t type, rewritten itohex function

--- src/core/hash/sha-1.c
+++ src/core/hash/sha-1.c
@@ -4,24 +4,32 @@
 #include <inttypes.h>
 
 typedef struct _uint160 {
-	uint8_t bitBuff[20];
+	uint32_t bitBuff[5];
 } uint160_t;
 
 static char *itohex(uint160_t val)
 {
 	static char symTable[17] = "0123456789abcdef";
+	static uint32_t masks[8] = {
+		0x0000000f, 0x000000f0,
+		0x00000f00, 0x0000f000,
+		0x000f0000, 0x00f00000,
+		0x0f000000, 0xf0000000
+	};
 
 	char *res = (char *)malloc(sizeof(char)*41);
 	int index = 40;
-	uint8_t rmask = 0x0f, lmask = 0xf0, value;
 
 	res[index--] = 0;
 
-	for (int i = 19; i >= 0; i--) {
-		value = val.bitBuff[i];
-		res[index--] = symTable[value & rmask];
-		res[index--] = symTable[(value & lmask) >> 4];
-	}
+	for (int i = 4; i >= 0; i--)
+		for (int j = 0; j < 8; j++)
+			res[index--] = symTable[(val.bitBuff[i] & masks[j]) >> 4*j];
 
 	return res;
+}
+
+char *GetHashCode(const char *const data)
+{
+	return NULL;
 }
